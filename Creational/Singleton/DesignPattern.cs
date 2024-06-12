a004bd1744a07bf4b5a468aa3de13cd300a709d9
Mert Can Kasapoğlu <mkasapoglu@guralcloud.com>
2024-06-12 11:44:02 +0300

    Creational Completed

---
+++ Creational/Singleton/DesignPattern.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    /*
+     * Senaryo: Mail sunucunuzun sadece bir tane olmasını istiyorsunuz. Bu durumda Singleton pattern kullanabilirsiniz.
+     */
+
+    //1. Adım: Singleton sınıfımızı oluşturuyoruz.
+
+    public class MailConfigurator
+    {
+        private string Host { get; set; }
+        private string Port { get; set; }
+        private bool EnableSsl { get; set; }
+
+        private static MailConfigurator _mailConfigurator;
+
+        private MailConfigurator()
+        {
+            Host = "mail.yourmailserver.com";
+            Port = "587";
+            EnableSsl = true;
+        }
+
+        public static MailConfigurator GetInstance()
+        {
+            if (_mailConfigurator == null)
+            {
+                _mailConfigurator = new MailConfigurator();
+            }
+
+            return _mailConfigurator;
+        }
+    }
+}
--- Creational/Singleton/Program.cs
+++ Creational/Singleton/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Singleton;
+
 Console.WriteLine("Hello, World!");
+
+/*
+ * Problem: Uygulamanızda sadece bir tane olmasını istediğiniz bir instance'ınız var. Bunu nasıl sağlarsınız?
+ * 
+ * Cevap: Singleton pattern kullanarak. Singleton pattern, bir sınıfın yalnızca bir örneğine sahip olmasını 
+ * sağlar ve bu örneğe global erişim sağlar.
+ * 
+ * 
+ */
+
+MailConfigurator mailConfigurator1 = MailConfigurator.GetInstance();
+MailConfigurator mailConfigurator2 = MailConfigurator.GetInstance();
+
+if (mailConfigurator1 == mailConfigurator2)
+{
+    Console.WriteLine("İki instance aynıdır.");
+}
+else
+{
+    Console.WriteLine("İki instance farklıdır.");
+}
