88e3ee5cb9ffc8548d3fd17dde7b83a62d7b3a0d
Mert Can <mert.kasapoglu@gurok.com>
2024-06-12 15:24:09 +0300

    Decorator Completed

---
+++ Structural/Decorator/DesignPattern.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    /*
+     * 
+     */
+
+    public interface IMail
+    {
+        void Send();
+    }
+
+    public class Mail : IMail
+    {
+        public void Send()
+        {
+            Console.WriteLine("Mail sent.");
+        }
+    }
+
+    public abstract class MailDecorator : IMail
+    {
+        protected IMail mail;
+
+        public MailDecorator(IMail mail)
+        {
+            this.mail = mail;
+        }
+
+        public virtual void Send()
+        {
+            mail.Send();
+        }
+    }
+
+    public class MailWithAttachment : MailDecorator
+    {
+        public MailWithAttachment(IMail mail) : base(mail)
+        {
+        }
+
+        public override void Send()
+        {
+            Console.WriteLine("Attachment added.");
+            base.Send();
+        }
+    }
+
+    public class MailWithSignature : MailDecorator
+    {
+        public MailWithSignature(IMail mail) : base(mail)
+        {
+        }
+
+        public override void Send()
+        {
+            Console.WriteLine("Signature added.");
+            base.Send();
+        }
+    }
+
+   
+}
---
+++ Structural/Decorator/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Decorator;
+using System.IO.Compression;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+
+Console.WriteLine("Hello, World!");
+
+//Stream fstream = new FileStream("data.txt", FileMode.Create);
+//Stream mstream = new MemoryStream();
+//Stream nstrean = new NetworkStream(new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp));
+// Decorate the stream
+
+//GZipStream stream = new GZipStream(fstream, CompressionLevel.Optimal);
+//CryptoStream cstream = new CryptoStream(stream, new AesManaged().CreateEncryptor(), CryptoStreamMode.Write);
+
+
+/*
+ * Problem: Hali hazırda var olan bir nesneye yeni özellikler eklemek istiyoruz.
+ *          Bunun için nesneyi değiştirmek istemiyoruz.
+ *          
+ *  Çözüm: Decorator Pattern
+ */
+
+IMail mail = new Mail();
+mail.Send();
+
+Console.WriteLine();
+
+IMail mailWithAttachment = new MailWithAttachment(mail);
+mailWithAttachment.Send();
+
+Console.WriteLine();
+
+IMail mailWithSignature = new MailWithSignature(mail);
+mailWithSignature.Send();
+
+Console.WriteLine();
+
+IMail mailWithAttachmentAndSignature = new MailWithSignature(new MailWithAttachment(mail));
+mailWithAttachmentAndSignature.Send();
